616fbb93e14f23503063afacc64d1a3b0ee0564f
Mrigank  Anand <mrigank.rkee@rediffmail.com>
2021-01-04 23:05:05 +0530

    adding new models

--- nimbusBackend/backend/models.py
+++ nimbusBackend/backend/models.py
@@ -1,24 +1,31 @@
-from django.db import models
-
+from django.db.models import CASCADE
 from django.db.models import Model
-from django.db.models.fields import CharField, DateTimeField, EmailField, IntegerField, URLField
+from django.db.models.fields import CharField, DateTimeField, EmailField, IntegerField, URLField, TextField
 from django.db.models.fields.related import ForeignKey
+
 
 # Create your models here.
 
 class User(Model):
+    """
+    User Details
+    """
     firebase = CharField('firebaseID', max_length=128, null=False, unique=True,
                          primary_key=True, blank=False)
 
     username = CharField('username', max_length=255, null=False, unique=True,
                          blank=False)
 
-    phone = CharField('phone', max_length=13)
-    
+    phone = CharField('phone', max_length=13, null=False, unique=False)
+
     email = EmailField('email', unique=True)
 
     firstName = CharField('firstName', max_length=255, null=False, blank=False)
     lastName = CharField('lastName', max_length=255, blank=True, null=True)
+
+    def __str__(self) -> str:
+        return self.username
+
 
 class Department(Model):
     name = CharField('name', max_length=255, null=False, unique=True, primary_key=True)
@@ -29,27 +36,96 @@ class Department(Model):
     def __str__(self) -> str:
         return "Department/Club: " + self.name
 
+
 class Quiz(Model):
+    """
+    Quiz
+    """
     name = CharField('name', max_length=255, null=False)
 
     count = IntegerField('count', default=0)
     sendCount = IntegerField('sendCount', default=1)
 
     startTime = DateTimeField('startTime', null=False,
-                               help_text='Format: YYYY-MM-DDThh:mm, example 2021-01-01T15:30')
+                              help_text='Format: YYYY-MM-DDThh:mm, example 2021-01-01T15:30')
     endTime = DateTimeField('endTime', null=False,
-                               help_text='Format: YYYY-MM-DDThh:mm, example 2021-01-01T15:30')
+                            help_text='Format: YYYY-MM-DDThh:mm, example 2021-01-01T15:30')
 
-    department = ForeignKey(Department, on_delete=models.CASCADE)
+    department = ForeignKey(Department, on_delete=CASCADE)
 
     def __str__(self):
         return "Quiz:" + self.name
 
+
 class Question(Model):
+    """
+    Quiz Question Details
+    """
     text = CharField('text', max_length=512)
     option1 = CharField('option1', max_length=256)
     option2 = CharField('option2', max_length=256)
     option3 = CharField('option3', max_length=256)
     option4 = CharField('option4', max_length=256)
-    
-    quiz = ForeignKey(Quiz, on_delete=models.CASCADE)
+
+    quiz = ForeignKey(Quiz, on_delete=CASCADE)
+
+
+class Schedule(Model):
+    """
+    Schedule by various departments
+    """
+    name = CharField('scheduleName', max_length=256, null=False, blank=False)
+    info = TextField('info', max_length=1000)
+    platform = CharField('platform', max_length=256)
+    date = DateTimeField('date', null=False, blank=False,
+                         help_text='Format: YYYY-MM-DDThh:mm, example 2021-01-01T15:30')
+    department = ForeignKey(Department, on_delete=CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Sponsor(Model):
+    """
+    Sponsor details
+    """
+    name = CharField('sponsorName', max_length=256, unique=True, null=False, blank=False)
+    image = URLField('imageUrl', max_length=256)
+
+    def __str__(self):
+        return self.name
+
+
+class Event(Model):
+    """
+    Event
+    """
+    choices = (
+        (0, "DepartmentEvent"),
+        (1, "InstituteEvent"),
+        (2, "Talk"),
+        (3, "Exhibition"),
+        (4, "Workshop")
+    )
+    name = CharField('EventName', max_length=256)
+    info = CharField('info', max_length=256)
+    platform = CharField('platform', max_length=256)
+    date = DateTimeField('date')
+    image = URLField('imageUrl', max_length=256)
+    abstract = TextField('abstract', max_length=1000)
+    type = IntegerField('type', max_length=256, choices=choices)
+
+    def __str__(self):
+        return self.name
+
+
+class Member(Model):
+    """
+    Member of App Team
+    """
+    name = CharField('memberName', max_length=256, null=False, blank=False)
+    position = CharField('position', max_length=256, null=False, blank=False)
+    image = URLField('imageUrl', max_length=256, null=False, blank=False)
+
+    def __str__(self):
+        return self.name
--- nimbusBackend/backend/views.py
+++ nimbusBackend/backend/views.py
@@ -1,6 +1,6 @@
 from django.db.models import query
 from django.http.response import Http404
-from backend.models import Department, User, Quiz, Question
+from .models import Department, User, Quiz, Question
 
 from rest_framework.response import Response
 from rest_framework.request import Request
@@ -9,9 +9,10 @@ from rest_framework.generics import GenericAPIView
 
 from rest_framework.mixins import CreateModelMixin, DestroyModelMixin, ListModelMixin, UpdateModelMixin
 
-from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED,HTTP_400_BAD_REQUEST, HTTP_500_INTERNAL_SERVER_ERROR
+from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST, HTTP_500_INTERNAL_SERVER_ERROR
 
-from backend.serializers import DepartmentSerializer, UserSerializer, QuizSerializer, QuestionSerializer
+from .serializers import DepartmentSerializer, UserSerializer, QuizSerializer, QuestionSerializer
+
 
 # Create your views here.
 
@@ -26,8 +27,8 @@ class UsersView(GenericAPIView):
         """
         users = self.get_queryset()
         return Response(users.data)
-    
-    def post(self, request:Request):
+
+    def post(self, request: Request):
         """
         Creates a user, if not already exists
         """
@@ -36,60 +37,63 @@ class UsersView(GenericAPIView):
             if user.is_valid():
                 user.save()
                 return Response(data=user.data, status=HTTP_201_CREATED)
-            
             else:
-                data = {"Message":"Invalid user data", "Errors:":user.errors}
-                return Response(data=data,status=HTTP_400_BAD_REQUEST)
-        
+                data = {"Message": "Invalid user data", "Errors:": user.errors}
+                return Response(data=data, status=HTTP_400_BAD_REQUEST)
+
         except Exception as e:
             print(e)
             return Response(status=HTTP_500_INTERNAL_SERVER_ERROR)
+
 
 class UserView(GenericAPIView, UpdateModelMixin, DestroyModelMixin):
     serializer_class = UserSerializer
 
     def get_queryset(self, pk=None):
-        try: 
-            if pk==None:
+        try:
+            if pk == None:
                 return User.objects.all()
             return User.objects.get(pk=pk)
         except User.DoesNotExist:
             raise Http404
-    def get(self, request:Request, pk):
+
+    def get(self, request: Request, pk):
         """
         Returns user with given firebase id
         """
         user = UserSerializer(self.get_queryset(pk))
         return Response(user.data)
-    
-    def put(self, request:Request, *args, **kwargs):
+
+    def put(self, request: Request, *args, **kwargs):
         """
         Updates user with given firebase id
         """
         return self.update(request, *args, **kwargs)
-    
-    def patch(self, request:Request, *args, **kwargs):
+
+    def patch(self, request: Request, *args, **kwargs):
         return self.partial_update(request, *args, **kwargs)
 
-    def delete(self, request:Request, *args, **kwargs):
+    def delete(self, request: Request, *args, **kwargs):
         return self.destroy(request, *args, **kwargs)
+
 
 class DepartmentsView(GenericAPIView, ListModelMixin, CreateModelMixin):
     serializer_class = DepartmentSerializer
 
     queryset = Department.objects.all()
 
-    def get(self, request:Request):
+    def get(self, request: Request):
         """
         Returns list of all departments
         """
         return self.list(request)
-    
-    def post(self, request:Request):
+
+    def post(self, request: Request):
         """
         Creates a department, if not exists
         """
         return self.create(request)
+
 
 class DepartmentView(GenericAPIView, ListModelMixin, UpdateModelMixin):
     serializer_class = DepartmentSerializer
@@ -101,14 +105,14 @@ class DepartmentView(GenericAPIView, ListModelMixin, UpdateModelMixin):
             return Department.objects.get(pk=pk)
         except Department.DoesNotExist:
             return Http404
-    
-    def get(self, request:Request, *args, **kwargs):
+
+    def get(self, request: Request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
-    
-    def put(self, request:Request, *args, **kwargs):
+
+    def put(self, request: Request, *args, **kwargs):
         return self.put(request, *args, **kwargs)
-    
-    def patch(self, request:Request, *args, **kwargs):
+
+    def patch(self, request: Request, *args, **kwargs):
         return self.partial_update(request, *args, **kwargs)
 
 
@@ -117,17 +121,18 @@ class QuizzesView(GenericAPIView, ListModelMixin, CreateModelMixin):
 
     queryset = Quiz.objects.all()
 
-    def get(self, request:Request):
+    def get(self, request: Request):
         """
         Returns list of all Quizs
         """
         return self.list(request)
-    
-    def post(self, request:Request):
+
+    def post(self, request: Request):
         """
         Creates a Quiz, if not exists
         """
         return self.create(request)
+
 
 class QuizView(GenericAPIView, ListModelMixin, UpdateModelMixin):
     serializer_class = QuizSerializer
@@ -139,32 +144,34 @@ class QuizView(GenericAPIView, ListModelMixin, UpdateModelMixin):
             return Quiz.objects.get(pk=pk)
         except Quiz.DoesNotExist:
             return Http404
-    
-    def get(self, request:Request, *args, **kwargs):
+
+    def get(self, request: Request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
-    
-    def put(self, request:Request, *args, **kwargs):
+
+    def put(self, request: Request, *args, **kwargs):
         return self.put(request, *args, **kwargs)
-    
-    def patch(self, request:Request, *args, **kwargs):
+
+    def patch(self, request: Request, *args, **kwargs):
         return self.partial_update(request, *args, **kwargs)
+
 
 class QuestionsView(GenericAPIView, ListModelMixin, CreateModelMixin):
     serializer_class = QuestionSerializer
 
     queryset = Question.objects.all()
 
-    def get(self, request:Request):
+    def get(self, request: Request):
         """
         Returns list of all Questions
         """
         return self.list(request)
-    
-    def post(self, request:Request):
+
+    def post(self, request: Request):
         """
         Creates a Question, if not exists
         """
         return self.create(request)
+
 
 class QuestionView(GenericAPIView, ListModelMixin, UpdateModelMixin):
     serializer_class = QuestionSerializer
@@ -176,12 +183,12 @@ class QuestionView(GenericAPIView, ListModelMixin, UpdateModelMixin):
             return Question.objects.get(pk=pk)
         except Question.DoesNotExist:
             return Http404
-    
-    def get(self, request:Request, *args, **kwargs):
+
+    def get(self, request: Request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
-    
-    def put(self, request:Request, *args, **kwargs):
+
+    def put(self, request: Request, *args, **kwargs):
         return self.put(request, *args, **kwargs)
-    
-    def patch(self, request:Request, *args, **kwargs):
+
+    def patch(self, request: Request, *args, **kwargs):
         return self.partial_update(request, *args, **kwargs)
